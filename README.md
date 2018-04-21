c0ff5feacca97f6ec7ac862cb9e59dd4439b4d23
bhavyaarora2 <helloworld09071994@gmail.com>
2018-04-21 11:53:44 +0530

    New Changes

--- README.md
+++
@@ -1,1 +0,0 @@
-# aidapp-web
---
+++ evenodd.php
@@ -0,0 +1,25 @@
+<html>
+<body>
+
+
+
+
+
+<?
+$num=10;
+if($num%2==0)
+{
+  echo"even number";
+
+}
+else{echo"odd";}
+
+?>
+<form method="post" action="evenodd.php">
+  ENTER THE NUMBER:<input type="text" name="odd" />
+  <input type="submit">
+
+</form>
+</body>
+
+</html>
